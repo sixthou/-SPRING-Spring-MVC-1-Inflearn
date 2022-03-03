79ce73281fb4aa2d77a1befbf772a80978437dc4
sixthou <justyuk@naver.com>
2022-03-03 20:55:34 +0900

    feat : Model 추가

--- src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberListControllerV2.java
+++ src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberListControllerV2.java
@@ -6,7 +6,6 @@ import hello.servlet.web.frontcontroller.MyView;
 import hello.servlet.web.frontcontroller.v2.ControllerV2;
 import java.io.IOException;
 import java.util.List;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
